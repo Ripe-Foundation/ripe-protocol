3fc80a383d18dad06307db55f968669da04c15a0
Rocko-O-Rama <diogo.perillo@gmail.com>
2025-06-11 01:01:39 -0600

    Final deploy adjustments

--- migrations/base-sepolia/1007_PriceDesk.py
+++ migrations/base-sepolia/1007_PriceDesk.py
@@ -75,6 +75,8 @@ def migrate(migration: Migration):
 
     migration.execute(mock_price_source.setPrice, migration.get_address("GreenToken"), EIGHTEEN_DECIMALS)
     migration.execute(mock_price_source.setPrice, migration.get_address("RipeToken"), EIGHTEEN_DECIMALS // 10)
+    migration.execute(price_desk.startAddNewAddressToRegistry, mock_price_source, "Mock Price Source")
+    migration.execute(price_desk.confirmNewAddressToRegistry, mock_price_source)
 
     mock_s_green_price = migration.deploy(
         "MockSGreenPrice",
